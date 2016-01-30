25594d4a6d1e3393c8235b05c4b71c3076c3b35c
Lowe Schmidt <github@loweschmidt.se>
2016-01-30 22:12:30 +0100

    Bundle up that oh-my-fish

---
+++ config/config.fish
@@ -0,0 +1,11 @@
+# Path to Oh My Fish install.
+set -gx OMF_PATH "/Users/lsc/.local/share/omf"
+
+# Customize Oh My Fish configuration path.
+#set -gx OMF_CONFIG "/Users/lsc/.config/omf"
+# Load oh-my-fish configuration.
+source $OMF_PATH/init.fish
+
+alias vim=nvim
+export GOPATH={$HOME}/go
+
