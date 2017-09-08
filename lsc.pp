0dca06a41aca4e35b8cfe5ba07b9c96bf8e33da8
Lowe Schmidt <github@loweschmidt.se>
2017-09-08 15:36:59 +0200

    initial bootstrapping scripts and manifests

---
+++ lsc.pp
@@ -0,0 +1,37 @@
+node default {
+  class { lsc: }
+}
+
+class lsc {
+
+  Package <| provider == tap |> -> Package <| provider == homebrew |>
+  Package <| provider == tap |> -> Package <| provider == brew |>
+  Package <| provider == tap |> -> Package <| provider == brewcask |>
+
+  $cask_pkg_list = [ '1password', 'slack', 'caffeine', 'google-drive', 'mpv', 'alfred',
+                      'iterm2-beta', 'karabiner-elements', 'google-chrome', 'spotify' ]
+
+  $pkg_list = [ 'awscli', 'consul', 'ctags', 'docker', 'docker-machine', 'docker-machine-driver-xhyve',
+                'git', 'hugo', 'neovim', 'nomad', 'packer', 'rbenv', 'rcm', 'terraform', 'unrar', 'vault' ]
+
+
+  package { [ "thoughtbot/formulae", "neovim/neovim" ]:
+    ensure   => present,
+    provider => tap,
+  }
+
+  package { $cask_pkg_list:
+    ensure   => present,
+    provider => brewcask,
+  }
+
+  package { $pkg_list:
+    ensure   => present,
+    provider => brew,
+  }
+
+  exec { 'osx_defaults':
+    command => '/bin/sh ./osx',
+  }
+
+}
