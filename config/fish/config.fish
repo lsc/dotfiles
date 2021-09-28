dacb71a08667e586cd3b5f6318b5b8f9691e6db8
Lowe Schmidt <github@loweschmidt.se>
2021-09-28 14:03:41 +0200

    add gcl alias

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,11 +1,10 @@
 #!/usr/bin/env fish
 
-set -x PATH /opt/homebrew/bin ~/bin ~/go/bin $PATH
+set -x PATH /opt/homebrew/opt/make/libexec/gnubin /opt/homebrew/bin ~/go/bin ~/.cargo/bin ~/bin $PATH
 set -u EDITOR nvim
 set -x TERRAGRUNT_DOWNLOAD ~/.terragrunt-cache
 set -U fish_greeting
 test -d $TERRAGRUNT_DOWNLOAD || mkdir -p $TERRAGRUNT_DOWNLOAD
 starship init fish | source
-test -r ~/.github-token && source ~/.github-token &> /dev/null
-source /opt/homebrew/opt/asdf/asdf.fish
-fish_add_path /opt/homebrew/opt/node@14/bin
+source "/opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc"
+fish_add_path /opt/homebrew/opt/openjdk@11/bin
---
+++ config/fish/functions/gcl.fish
@@ -0,0 +1,4 @@
+#!/usr/bin/env fish
+function gcl
+    gcloud $argv
+end
