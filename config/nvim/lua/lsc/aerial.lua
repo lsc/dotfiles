3629ba2eca3248bb7120f43799e24f334f16b9ce
Lowe Schmidt <github@loweschmidt.se>
2022-01-16 18:56:56 +0100

    Add aerial to statusline

--- config/nvim/lua/lsc/aerial.lua
+++ config/nvim/lua/lsc/aerial.lua
@@ -8,9 +8,17 @@ end
 aerial.setup {}
 
 local tele_status_ok, telescope = pcall(require, "telescope")
+local lsp_status_ok, lspconfig = pcall(require, "lspconfig")
+
+if not lsp_status_ok then
+  return
+end
 
 if not tele_status_ok then
   return
 end
 
 telescope.load_extension('aerial')
+lspconfig.vimls.setup{
+  on_attach = require("aerial").on_attach
+}
