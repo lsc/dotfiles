88f8a4db49b54121f03cd1ea19f9b3f654897d8d
Lowe Schmidt <github@loweschmidt.se>
2022-03-22 12:31:00 +0100

    Remove redundant setting

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,15 +2,13 @@
 
 set --export EDITOR nvim
 set --export TERRAGRUNT_DOWNLOAD ~/.terragrunt-cache
-# We build our Kotlin projects on Java 11 at M
-set --export JAVA_HOME /opt/homebrew/opt/openjdk@11/
 set --unexport fish_greeting
 
 set add_paths "~/go/bin ~/.cargo/bin ~/bin"
 
 if command -v brew &> /dev/null
     set brew_prefix (brew --prefix)
-    fish_add_path "$brew_prefix/opt/openjdk@11/bin" "$brew_prefix/opt/make/libexec/gnubin" "$brew_prefix/bin" "$brew_prefix/sbin"
+    fish_add_path "$brew_prefix/opt/make/libexec/gnubin" "$brew_prefix/bin" "$brew_prefix/sbin"
     set --export HOMEBREW_NO_ENV_HINTS 1
 end
 
@@ -23,6 +21,5 @@ source ~/.github-token
 source /opt/homebrew/opt/asdf/libexec/asdf.fish
 
 set -x SSH_AUTH_SOCK /Users/lowe.schmidt/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh
-eval (driftctl completion fish)
 command -v starship &> /dev/null && starship init fish | source
 
