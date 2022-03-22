3052d46b008a27323463b0539813048b05578210
Lowe Schmidt <github@loweschmidt.se>
2022-03-22 13:07:53 +0100

    check if program is installed before sourcing config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -5,9 +5,8 @@ set --export TERRAGRUNT_DOWNLOAD ~/.terragrunt-cache
 set --export JAVA_HOME (dirname (dirname (asdf which java)))/
 set --unexport fish_greeting
 
-source /opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
-source ~/.github-token
-source /opt/homebrew/opt/asdf/libexec/asdf.fish
+command -v gcloud &>/dev/null && source /opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
+command -v asdf &> /dev/null && source /opt/homebrew/opt/asdf/libexec/asdf.fish
 
 set -x SSH_AUTH_SOCK /Users/lowe.schmidt/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh
 command -v starship &> /dev/null && starship init fish | source
