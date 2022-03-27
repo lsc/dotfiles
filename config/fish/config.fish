9f37dbb7f4071d72570d198ec4069f2f3dc0c658
Lowe Schmidt <github@loweschmidt.se>
2022-03-27 09:59:48 +0000

    asdf installs differ between mac and linux

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,12 +2,19 @@
 
 set --export EDITOR nvim
 set --export TERRAGRUNT_DOWNLOAD ~/.terragrunt-cache
-set --export JAVA_HOME (dirname (dirname (asdf which java)))/
 set --unexport fish_greeting
 
-command -v gcloud &>/dev/null && source /opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
-command -v asdf &> /dev/null && source /opt/homebrew/opt/asdf/libexec/asdf.fish
+set -x PATH $HOME/.cargo/bin $HOME/.asdf/shims $HOME/.asdf/bin $PATH
 
+command -v gcloud &>/dev/null && source /opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
+
+if command -v asdf == $HOME/.asdf/bin/asdf
+  source $HOME/.asdf/asdf.fish
+else if command -v &> /dev/null asdf == /opt/homebrew/opt/asdf/libexec/bin/asdf
+  source /opt/homebrew/opt/asdf/libexec/asdf.fish
+end
+
+set --export JAVA_HOME (dirname (dirname (asdf which java)))/
 set -x SSH_AUTH_SOCK /Users/lowe.schmidt/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh
 command -v starship &> /dev/null && starship init fish | source
 
