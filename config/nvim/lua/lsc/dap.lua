725adc5c79e53455fb0dbcbeca7bc37f116fca89
Lowe Schmidt <github@loweschmidt.se>
2022-06-18 11:07:59 +0200

    Dap go!

---
+++ config/nvim/lua/lsc/dap.lua
@@ -0,0 +1,14 @@
+local dgo_status_ok, dgo = pcall(require, 'dap-go')
+
+if not dgo_status_ok then
+  return
+end
+
+local dui_status_ok, dui = pcall(require, 'dapui')
+
+if not dui_status_ok then
+  return
+end
+
+dgo.setup()
+dui.setup()
