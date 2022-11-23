51cbb81e0a1030d0977361eeb673e524809912be
Lowe Schmidt <github@loweschmidt.se>
2022-11-23 12:06:03 +0100

    Skaffold is for CD on kube

--- config/nvim/lua/lsc/keymaps.lua
+++ config/nvim/lua/lsc/keymaps.lua
@@ -63,7 +63,6 @@ keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
 keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
 keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
 keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
-
   
 -- Telescope
 keymap("n", "<leader>f", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
