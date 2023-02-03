95d602d3c58b9dd7fb82ec26dd089c1714949842
Lowe Schmidt <github@loweschmidt.se>
2023-02-03 19:56:35 +0100

    filetype fixes terraform?

---
+++ config/nvim/lua/plugins/filetype.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "nathom/filetype.nvim",
+    config = function()
+      require("filetype").setup({
+        overrides = {
+          extensions = {
+            tf = "terraform",
+            tfvars = "terraform",
+            tfstate = "json",
+          },
+        },
+      })
+    end,
+  },
+}
