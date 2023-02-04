cae32b64807cad5b2e63d0ba7a1c66f85fc52e58
Lowe Schmidt <github@loweschmidt.se>
2023-02-04 13:13:18 +0100

    format that

--- config/nvim/lua/plugins/neotree.lua
+++ config/nvim/lua/plugins/neotree.lua
@@ -1,13 +1,15 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  opts = {
-    use_libuv_file_watcher = true,
-    bind_to_cwd = true,
-    window = {
-      mappings = {
-        ["o"] = "open_with_window_picker",
-        ["s"] = "vsplit_with_window_picker",
-        ["S"] = "split_with_window_picker",
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      use_libuv_file_watcher = true,
+      bind_to_cwd = true,
+      window = {
+        mappings = {
+          ["o"] = "open_with_window_picker",
+          ["s"] = "vsplit_with_window_picker",
+          ["S"] = "split_with_window_picker",
+        },
       },
     },
   },
