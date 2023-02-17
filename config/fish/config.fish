a70f6518add4dfbca0461bc0048711a52c80edf2
Lowe Schmidt <github@loweschmidt.se>
2023-02-17 12:22:42 +0100

    Detect os and source correct config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,19 +1,36 @@
 #!/usr/bin/env fish
 set --export EDITOR nvim
 set --export USE_GKE_GCLOUD_AUTH_PLUGIN True
-
 set --unexport fish_greeting
+set --export STARSHIP_DISTRO ""
 
-set asdf (command -v asdf)
-source /opt/homebrew/opt/asdf/libexec/asdf.fish
+set os (uname)
+set shell (basename $SHELL)
+
+if (command -v asdf)
+    switch $os
+        case Darwin
+            source /opt/homebrew/opt/asdf/libexec/asdf.$shell
+        case Linux
+            source ~/.asdf/libexec/asdf.$shell
+        case '*'
+            true
+    end
+end
 
 if status is-interactive
-  atuin init fish | source
+  atuin init $shell | source
 end
 
 # Google Cloud SDK
-source "/opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc"
-set lfile "/etc/*-release"
-set mfile "/System/Library/CoreServices/SystemVersion.plist"
-set --export STARSHIP_DISTRO ""
+if (command -v gcloud)
+    switch $os
+        case Darwin
+            source "/opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.$shell.inc"
+        case '*'
+            true
+    end
+end
+            
+
 command -v starship &> /dev/null && starship init fish | source
