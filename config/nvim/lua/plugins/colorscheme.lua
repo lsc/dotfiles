7d4d2b5b3c75459294bb81e06a23c91c35090816
Lowe Schmidt <github@loweschmidt.se>
2023-02-22 19:58:22 +0100

    Use symbols rather than aerial, nice breakdwon of symbols in my opinion

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,12 @@
 return {
   { "rebelot/kanagawa.nvim" },
   {
+    "uloco/bluloco.nvim",
+    dependencies = {
+      "rktjmp/lush.nvim",
+    },
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "kanagawa",
---
+++ config/nvim/lua/plugins/symbols-outline.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "simrat39/symbols-outline.nvim",
+    config = true,
+    keys = {
+      { "<leader>a", ":SymbolsOutline <cr>", desc = "Toggle Symbols" },
+    },
+  },
+}
