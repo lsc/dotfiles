dec5872e74a5a9dac340e6d42016d332b299f0bd
Lowe Schmidt <github@loweschmidt.se>
2023-02-23 16:41:51 +0100

    Add debugger integration and ui

--- config/nvim/lua/plugins/symbols-outline.lua
+++ config/nvim/lua/plugins/symbols-outline.lua
@@ -2,6 +2,7 @@ return {
   {
     "stevearc/aerial.nvim",
     config = true,
+    cmd = "AerialToggle",
     keys = {
       { "<leader>m", ":AerialToggle<cr>", desc = "Toggle Aerial Outline" },
     },
@@ -12,6 +13,7 @@ return {
   {
     "simrat39/symbols-outline.nvim",
     config = true,
+    cmd = "SymbolsOutline",
     keys = {
       { "<leader>a", ":SymbolsOutline <cr>", desc = "Toggle Symbols Outline" },
     },
