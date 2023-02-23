977cacba3924b337eda2a6a1b69fe74dc2f65d3a
Lowe Schmidt <github@loweschmidt.se>
2023-02-23 16:16:02 +0100

    Readd toggleterm, as they can be hidden but keep running in contrast to the builtin lazyvim one

--- config/nvim/lua/plugins/symbols-outline.lua
+++ config/nvim/lua/plugins/symbols-outline.lua
@@ -1,9 +1,22 @@
 return {
   {
+    "stevearc/aerial.nvim",
+    config = true,
+    keys = {
+      { "<leader>m", ":AerialToggle<cr>", desc = "Toggle Aerial Outline" },
+    },
+    opts = {
+      filter_kind = { false },
+    },
+  },
+  {
     "simrat39/symbols-outline.nvim",
     config = true,
     keys = {
-      { "<leader>a", ":SymbolsOutline <cr>", desc = "Toggle Symbols" },
+      { "<leader>a", ":SymbolsOutline <cr>", desc = "Toggle Symbols Outline" },
+    },
+    opts = {
+      keymaps = {},
     },
   },
 }
