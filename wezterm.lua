79dd313106a55fb43975feeff7f9f7adf66d110d
Lowe Schmidt <github@loweschmidt.se>
2023-03-13 07:45:30 +0100

    use the fancy tab bar in wezterm

--- wezterm.lua
+++ wezterm.lua
@@ -1,21 +1,17 @@
 local wezterm = require("wezterm")
-local act = wezterm.action
 
 return {
 	send_composed_key_when_left_alt_is_pressed = true,
 	swap_backspace_and_delete = false,
 	hide_tab_bar_if_only_one_tab = true,
-	use_fancy_tab_bar = false,
+	use_fancy_tab_bar = true,
 	show_update_window = false,
 	font = wezterm.font({
 		family = "Jetbrains Mono NL",
 		weight = "DemiLight",
 		harfbuzz_features = { "zero", "onum", "ss02", "ss03", "ss04", "ss05", "ss08" },
 	}),
-	-- keys = {
-	-- 	{ key = "t", mods = "SUPER", action = act.SpawnTab("DefaultDomain") },
-	-- },
-	font_size = 18,
+	font_size = 17,
 	color_scheme = "kanagawabones",
 	window_padding = {
 		left = 0,
