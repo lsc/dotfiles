60e46d4bdae27880c520cd38fe549d2bf965162b
Lowe Schmidt <github@loweschmidt.se>
2023-11-07 14:56:28 +0100

    Slightly more advanced project/session management

--- config/nvim-lazy/lua/config/lazy.lua
+++ config/nvim-lazy/lua/config/lazy.lua
@@ -23,7 +23,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.ui.edgy" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
-    { import = "lazyvim.plugins.extras.util.project" },
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     -- { import = "lazyvim.plugins.extras.coding.copilot" },
     -- import/override with your plugins
