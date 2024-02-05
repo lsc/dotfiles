3e7e0e31f3f23e2088a2cf82dce14f6957c323b9
Lowe Schmidt <github@loweschmidt.se>
2024-02-05 14:52:54 +0100

    cue :rocket:

--- wezterm.lua
+++ wezterm.lua
@@ -14,9 +14,9 @@ return {
 	font_size = 17,
 	color_scheme = "kanagawabones",
 	window_padding = {
-		left = 0,
-		right = 0,
-		top = 0,
-		bottom = 0,
+		left = 20,
+		right = 20,
+		top = 10,
+		bottom = 10,
 	},
 }
