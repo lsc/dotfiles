990aba25d78f260ece626ff79fcc49740f1c92ea
Lowe Schmidt <github@loweschmidt.se>
2024-05-13 15:18:00 +0200

    blarhg

--- config/fish/functions/jd.fish
+++ config/fish/functions/jd.fish
@@ -1,9 +1,3 @@
 function jd
-<<<<<<< Updated upstream
     cd (fd -d 4 -t d . ~/src | fzf)
-||||||| Stash base
-    cd (fd -t d . ~/src | fzf)
-=======
-    cd (fd -t d -d 2 . ~/src | fzf)
->>>>>>> Stashed changes
 end
