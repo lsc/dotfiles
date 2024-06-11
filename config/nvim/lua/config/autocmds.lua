89a50198b3ef29091f3cf607d14945cbd942618e
Lowe Schmidt <github@loweschmidt.se>
2024-06-11 14:31:05 +0200

    yehaw

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -1,6 +1,7 @@
 -- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+-- Default autocmds that are always set: https://github.com//LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
 vim.api.nvim_create_autocmd("FileType", {
   pattern = { "python" },
   callback = function()
