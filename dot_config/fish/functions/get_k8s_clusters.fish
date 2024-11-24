5706cded5cc29165320ad5daf1a4112081557446
Lowe Schmidt <github@loweschmidt.se>
2024-11-24 14:12:34 +0100

    stuff

--- dot_config/fish/functions/get_k8s_clusters.fish
+++ dot_config/fish/functions/get_k8s_clusters.fish
@@ -4,9 +4,8 @@ function get_k8s_clusters --argument-names env
     is_installed curl || exit 1
     is_installed kubectl || exit 1
 
-    set --local out $HOME/.kube/k8sfactsconfig-$env.yaml
-    set --local user lowe.schmidt
-    set --local base_url "https://k8sfacts{{ENV}}.imperva-services.net/kubeconfig?role=full-admin&cluster_role_binding=true&user=$user"
+    set --local out $HOME/.kube/static/k8sfactsconfig-$env.yaml
+    set --local base_url "https://k8sfacts{{ENV}}.imperva-services.net/kubeconfig?role=full-admin&cluster_role_binding=true&user=lowe.schmidt"
 
     if string match $env prd
         set endpoint (string replace {{ENV}} "" $base_url)
@@ -16,7 +15,4 @@ function get_k8s_clusters --argument-names env
 
     curl -sS $endpoint -o $out &>/dev/null
     chmod 600 $out
-
-    set --append KUBECONFIG $out
-    set --export KUBECONFIG
 end
