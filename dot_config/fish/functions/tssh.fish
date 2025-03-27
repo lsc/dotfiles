b4649ac357e1cd232a5176f959f53e2f0e1a8c5f
Lowe Schmidt <github@loweschmidt.se>
2025-03-27 16:12:29 +0100

    fix shit

--- dot_config/fish/functions/tssh.fish
+++ dot_config/fish/functions/tssh.fish
@@ -1,5 +1,4 @@
 #!/usr/bin/env fish
 function tssh --description "SSH to a teleport configured host"
-    _generate_teleport_host_list
-    tsh ssh (fzf < $teleport_host_list)
+    tsh ssh (fzf < $teleport_host_list) end
 end
