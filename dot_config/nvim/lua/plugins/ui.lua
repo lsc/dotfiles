4e142f78cc2ace603abbd23f4e3f9c56f3f636f1
Lowe Schmidt <github@loweschmidt.se>
2025-03-30 12:18:11 +0200

    spring cleaning

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -18,22 +18,4 @@ return {
       },
     },
   },
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    opts = {
-      use_libuv_file_watcher = true,
-      bind_to_cwd = true,
-      window = {
-        width = 600,
-        mappings = {
-          ["o"] = "open",
-          ["l"] = "open_with_window_picker",
-          ["s"] = "vsplit_with_window_picker",
-          ["S"] = "split_with_window_picker",
-          ["h"] = "close_node",
-          ["W"] = "close_all_nodes",
-        },
-      },
-    },
-  },
 }
--- dot_config/nvim/lua/plugins/utils.lua
+++ dot_config/nvim/lua/plugins/utils.lua
@@ -1,16 +1,5 @@
 return {
   {
-    "echasnovski/mini.align",
-    version = false,
-    opts = {},
-  },
-  {
-    "s1n7ax/nvim-window-picker",
-    opts = {
-      tag = "v2.*",
-    },
-  },
-  {
     "stevearc/oil.nvim",
     opts = {},
     -- Optional dependencies
@@ -21,13 +10,6 @@ return {
     cmd = "Neogit",
     keys = { { "<leader>gn", "<cmd>Neogit<cr>", desc = "Neogit" } },
     opts = { disable_commit_confirmation = true },
-  },
-  {
-    "Wansmer/treesj",
-    dependencies = { "nvim-treesitter/nvim-treesitter" },
-    config = function()
-      require("treesj").setup({})
-    end,
   },
   {
     "sindrets/diffview.nvim",
@@ -41,9 +23,6 @@ return {
     config = true,
   },
   {
-    "esensar/nvim-dev-container",
-  },
-  {
     "vhyrro/luarocks.nvim",
     priority = 1000, -- Very high priority is required, luarocks.nvim should run as the first plugin in your config.
     config = true,
@@ -54,33 +33,5 @@ return {
     lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
     version = "*", -- Pin Neorg to the latest stable release
     config = true,
-  },
-  {
-    "epwalsh/obsidian.nvim",
-    version = "*", -- recommended, use latest release instead of latest commit
-    lazy = true,
-    ft = "markdown",
-    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-    -- event = {
-    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-    --   -- refer to `:h file-pattern` for more examples
-    --   "BufReadPre path/to/my-vault/*.md",
-    --   "BufNewFile path/to/my-vault/*.md",
-    -- },
-    dependencies = {
-      -- Required.
-      "nvim-lua/plenary.nvim",
-
-      -- see below for full list of optional dependencies 👇
-    },
-    opts = {
-      workspaces = {
-        {
-          name = "personal",
-          path = "/mnt/c/Users/lsc/iCloudDrive/iCloud~md~obsidian/",
-        },
-      },
-    },
   },
 }
