5ebf45f5ec83450a003c00f6c72424b42337901c
Lowe Schmidt <github@loweschmidt.se>
2025-04-04 09:39:18 +0200

    search and replace already part of lazy, have to do python at $dayjob...

--- dot_config/nvim/lua/plugins/utils.lua
+++ dot_config/nvim/lua/plugins/utils.lua
@@ -19,10 +19,6 @@ return {
     "f-person/git-blame.nvim",
   },
   {
-    "AckslD/muren.nvim",
-    config = true,
-  },
-  {
     "vhyrro/luarocks.nvim",
     priority = 1000, -- Very high priority is required, luarocks.nvim should run as the first plugin in your config.
     config = true,
