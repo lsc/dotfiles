9c7275a76ad8bba1b329b7a8486b8b50e8bb83aa
Lowe Schmidt <github@loweschmidt.se>
2025-09-25 09:13:38 +0200

    coolio

--- dot_config/nvim/lua/plugins/utils.lua
+++ dot_config/nvim/lua/plugins/utils.lua
@@ -19,18 +19,6 @@ return {
     "f-person/git-blame.nvim",
   },
   {
-    "vhyrro/luarocks.nvim",
-    priority = 1000, -- Very high priority is required, luarocks.nvim should run as the first plugin in your config.
-    config = true,
-  },
-  {
-    "nvim-neorg/neorg",
-    dependencies = { "vhyrro/luarocks.nvim" },
-    lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
-    version = "*", -- Pin Neorg to the latest stable release
-    config = true,
-  },
-  {
     "benomahony/uv.nvim",
     opts = {
       picker_integration = true,
