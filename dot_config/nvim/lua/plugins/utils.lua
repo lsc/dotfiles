edc68802d7ffe00b87545e7748e4cacedff5157b
Lowe Schmidt <github@loweschmidt.se>
2025-10-24 17:13:07 +0200

    coolio

--- dot_config/nvim/lua/plugins/utils.lua
+++ dot_config/nvim/lua/plugins/utils.lua
@@ -9,6 +9,9 @@ return {
     "NicolasGB/jj.nvim",
   },
   {
+    "evanphx/jjsigns.nvim",
+  },
+  {
     "sindrets/diffview.nvim",
     dependencies = "nvim-lua/plenary.nvim",
   },
@@ -42,16 +45,6 @@ return {
     "obsidian-nvim/obsidian.nvim",
     version = "*", -- recommended, use latest release instead of latest commit
     ft = "markdown",
-    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-    -- event = {
-    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-    --   -- refer to `:h file-pattern` for more examples
-    --   "BufReadPre path/to/my-vault/*.md",
-    --   "BufNewFile path/to/my-vault/*.md",
-    -- },
-    ---@module 'obsidian'
-    ---@type obsidian.config
     opts = {
       workspaces = {
         {
@@ -59,8 +52,6 @@ return {
           path = "~/Sources/Obsidian/",
         },
       },
-
-      -- see below for full list of options 👇
     },
   },
 }
