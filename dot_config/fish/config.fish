ff0ded4b9f210997ef5338dc0ba84415c062c1e2
Lowe Schmidt <git@loweschmidt.se>
2026-07-01 00:20:39 +0200

    coolio

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -81,3 +81,11 @@ alias tg terragrunt
 alias tm terramate
 alias v nvim
 alias zj zellij
+
+# BEGIN opam configuration
+# This is useful if you're using opam as it adds:
+#   - the correct directories to the PATH
+#   - auto-completion for the opam binary
+# This section can be safely removed at any time if needed.
+test -r ~/.opam/opam-init/init.fish && source ~/.opam/opam-init/init.fish >/dev/null 2>/dev/null; or true
+# END opam configuration
