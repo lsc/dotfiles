d4cd63e082f04d0e1cf35cdcc5ee78391a71aa69
Lowe Schmidt <git@loweschmidt.se>
2026-09-22 08:44:08 +0200

    coolio

--- dot_config/nvim/lua/plugins/databases.lua
+++ dot_config/nvim/lua/plugins/databases.lua
@@ -2,4 +2,21 @@ return {
   { "tpope/vim-dadbod" },
   { "kristijanhusak/vim-dadbod-ui" },
   { "kristijanhusak/vim-dadbod-completion" },
+  {
+    "2giosangmitom/sqmeow.nvim",
+    dependencies = { "MunifTanjim/nui.nvim" },
+    version = "*",
+    build = function()
+      -- Downloads the matching release binary; pass 'curl', 'wget', 'powershell' or 'cargo' to choose.
+      require("sqmeow").install()
+    end,
+    opts = {},
+    cmd = "Sqmeow",
+    keys = {
+      { "<leader>Dd", "<cmd>Sqmeow toggle<cr>", desc = "Toggle" },
+      { "<leader>Dc", "<cmd>Sqmeow cancel<cr>", desc = "Cancel" },
+      { "<leader>Da", "<cmd>Sqmeow add<cr>", desc = "Add Connection" },
+      { "<leader>Ds", "<cmd>Sqmeow scratch<cr>", desc = "New Scratchpad" },
+    },
+  },
 }
