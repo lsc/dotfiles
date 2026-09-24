678c240345c5e8f580a16eb53999ae848942c852
Lowe Schmidt <git@loweschmidt.se>
2026-09-24 08:06:56 +0200

    coolio

--- dot_config/nvim/lua/plugins/languages.lua
+++ dot_config/nvim/lua/plugins/languages.lua
@@ -34,4 +34,10 @@ return {
   {
     "let-def/texpresso.nvim",
   },
+  {
+    "blackhat-7/vellum.nvim",
+    ft = "markdown",
+    keys = { { "<leader>mp", "<cmd>Vellum<cr>", desc = "Markdown preview" } },
+    opts = {},
+  },
 }
